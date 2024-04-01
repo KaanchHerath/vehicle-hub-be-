801456809053641fbcabf5782e5344224b532285
NavodhSFernando <fernandons.21@uom.lk>
2024-04-02 00:07:36 +0530

    Maintenance service reconfigured

--- reservation-system-be/Services/MaintenanceTypeServices/IMaintenanceTypeService.cs
+++ reservation-system-be/Services/MaintenanceTypeServices/IMaintenanceTypeService.cs
@@ -5,9 +5,9 @@ namespace reservation_system_be.Services.MaintenanceTypeServices
     public interface IMaintenanceTypeService
     {
         Task<List<MaintenanceType>> GetAllMaintenanceType();
-        Task<VehicleMake?> GetSingleMaintenanceType(int id);
-        Task<List<VehicleMake>> AddMaintenanceType(MaintenanceType maintenanceType);
-        Task<List<VehicleMake>?> UpdateMaintenanceType(int id, MaintenanceType request);
-        Task<List<VehicleMake>?> DeleteMaintenanceType(int id);
+        Task<MaintenanceType?> GetSingleMaintenanceType(int id);
+        Task<List<MaintenanceType>> AddMaintenanceType(MaintenanceType maintenanceType);
+        Task<List<MaintenanceType>?> UpdateMaintenanceType(int id, MaintenanceType request);
+        Task<List<MaintenanceType>?> DeleteMaintenanceType(int id);
     }
 }
--- reservation-system-be/Services/MaintenanceTypeServices/MaintenanceTypeService.cs
+++ reservation-system-be/Services/MaintenanceTypeServices/MaintenanceTypeService.cs
@@ -6,6 +6,55 @@ namespace reservation_system_be.Services.MaintenanceTypeServices
 {
     public class MaintenanceTypeService : IMaintenanceTypeService
     {
+        private readonly DataContext _context;
 
+        public MaintenanceTypeService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MaintenanceType>> GetAllMaintenanceType()
+        {
+            return await _context.MaintenanceTypes.ToListAsync();
+        }
+
+        public async Task<MaintenanceType?> GetSingleMaintenanceType(int id)
+        {
+            return await _context.MaintenanceTypes.FindAsync(id);
+        }
+
+        public async Task<List<MaintenanceType>> AddMaintenanceType(MaintenanceType maintenanceType)
+        {
+            _context.MaintenanceTypes.Add(maintenanceType);
+            await _context.SaveChangesAsync();
+            return await _context.MaintenanceTypes.ToListAsync();
+        }
+
+        public async Task<List<MaintenanceType>?> UpdateMaintenanceType(int id, MaintenanceType request)
+        {
+            var maintenanceType = await _context.MaintenanceTypes.FindAsync(id);
+            if (maintenanceType == null)
+            {
+                return null;
+            }
+
+            maintenanceType.Name = request.Name;
+            maintenanceType.MaintenanceId = request.MaintenanceId;
+            await _context.SaveChangesAsync();
+            return await _context.MaintenanceTypes.ToListAsync();
+        }
+
+        public async Task<List<MaintenanceType>?> DeleteMaintenanceType(int id)
+        {
+            var maintenanceType = await _context.MaintenanceTypes.FindAsync(id);
+            if (maintenanceType == null)
+            {
+                return null;
+            }
+
+            _context.MaintenanceTypes.Remove(maintenanceType);
+            await _context.SaveChangesAsync();
+            return await _context.MaintenanceTypes.ToListAsync();
+        }
     }
 }
