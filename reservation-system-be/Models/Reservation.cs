8f6ef9dcb8f832b3556ffac978f6a6cb93955b05
NavodhSFernando <fernandons.21@uom.lk>
2024-04-20 13:13:25 +0530

    Reservation crud working

--- reservation-system-be/Models/Reservation.cs
+++ reservation-system-be/Models/Reservation.cs
@@ -1,4 +1,7 @@
-﻿namespace reservation_system_be.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace reservation_system_be.Models
 {
     public class Reservation
     {
@@ -7,10 +10,14 @@
             public DateTime EndTime { get; set; }
             public DateTime StartDate { get; set; }
             public DateTime EndDate { get; set; }
-            public string Status { get; set; } = string.Empty;
+            [JsonConverter(typeof(JsonStringEnumConverter))]
+            public Status Status { get; set; } = Status.Waiting;
             public VehicleLog? VehicleLog { get; set; }
             public Feedback? Feedback { get; set; } 
             public ICollection<Employee>? Employees { get; set; }
+            [ForeignKey("CustomerReservationId")]
+            public int CustomerReservationId { get; set; }
+            [JsonIgnore]
             public CustomerReservation? CusReservation { get; set; }
             public ICollection<Notification> Notification { get; } = new List<Notification>(); 
 
@@ -23,5 +30,13 @@
                 }
             }
     }
+    public enum Status
+    {
+        Waiting,
+        Pending,
+        Confirmed,
+        Cancelled
+    }
 }
+
 
--- reservation-system-be/Program.cs
+++ reservation-system-be/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using reservation_system_be.Data;
 using reservation_system_be.Services.VehicleMakeServices;
 using reservation_system_be.Services.VehicleTypeServices;
+using System.Text.Json.Serialization;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,10 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+ options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
