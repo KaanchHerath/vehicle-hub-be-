4dae5e88d9c794ade2d3b9d8c970a2c67d21c86e
NavodhSFernando <fernandons.21@uom.lk>
2024-04-22 15:45:45 +0530

    customer model

--- reservation-system-be/Models/Customer.cs
+++ reservation-system-be/Models/Customer.cs
@@ -12,7 +12,6 @@ namespace reservation_system_be.Models
         public bool Status { get; set; } = true;
         public int ContactNo { get; set; } 
         public string Address { get; set; } = string.Empty;
-        public int ContactNo { get; set; }
         public string Password { get; set; } = string.Empty;
         [JsonIgnore]
         public ICollection<Wishlist>? Wishlist { get; set; }
