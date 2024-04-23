dd977118908b4b8bb4907e7092b12ab09951e6de
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-04-23 11:45:04 +0530

    created vehicleLog APIs

---
+++ reservation-system-be/Services/VehicleLogServices/IVehicleLogService.cs
@@ -0,0 +1,13 @@
+﻿using reservation_system_be.Models;
+
+namespace reservation_system_be.Services.VehicleLogServices
+{
+    public interface IVehicleLogService
+    {
+        Task<List<VehicleLog>> GetAllVehicleLogs();
+        Task<VehicleLog> GetVehicleLog(int id);
+        Task<VehicleLog> CreateVehicleLog(VehicleLog vehicleLog);
+        Task<VehicleLog> UpdateVehicleLog(int id, VehicleLog vehicleLog);
+        Task DeleteVehicleLog(int id);
+    }
+}
