303d7ec3985d7c76344eccf19940596254f074f8
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-05-23 18:15:46 +0530

    updated Vehicle

---
+++ reservation-system-be/DTOs/VehicleDto.cs
@@ -0,0 +1,18 @@
+﻿using reservation_system_be.Models;
+
+namespace reservation_system_be.DTOs
+{
+    public record struct VehicleDto
+    (
+        int Id,
+        string RegistrationNumber,
+        string ChassisNo,
+        string Colour,
+        int Mileage,
+        float CostPerDay,
+        string Transmission,
+        VehicleType VehicleType,
+        VehicleModelDto VehicleModel,
+        Employee Employee
+    );
+}
