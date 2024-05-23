1ac9eb177a86caae4b85b2dded963fdf50a4cbd7
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-05-23 18:17:16 +0530

    updated VehicleInsurance

---
+++ reservation-system-be/DTOs/VehicleInsuranceDto.cs
@@ -0,0 +1,13 @@
+﻿using reservation_system_be.Models;
+
+namespace reservation_system_be.DTOs
+{
+    public record struct VehicleInsuranceDto
+    (
+        int Id,
+        string InsuranceNo,
+        DateTime ExpiryDate,
+        VehicleDto Vehicle,
+        bool Status
+    );
+}
