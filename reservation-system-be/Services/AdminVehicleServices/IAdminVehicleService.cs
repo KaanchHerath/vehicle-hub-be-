cca0f95724a8589a39b165715dde3da28d032e6d
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-05-26 20:28:45 +0530

    created AdminVehicle Service

---
+++ reservation-system-be/Services/AdminVehicleServices/IAdminVehicleService.cs
@@ -0,0 +1,12 @@
+﻿using reservation_system_be.DTOs;
+
+namespace reservation_system_be.Services.AdminVehicleServices
+{
+    public interface IAdminVehicleService
+    {
+        Task CreateVehicleModel(CreateVehicleModelDto createVehicleModelDto);
+        Task<IEnumerable<AdditionalFeaturesDto>> ViewVehicleModels();
+        Task UpdateVehicleModel(int id, CreateVehicleModelDto createVehicleModelDto);
+        Task<AdditionalFeaturesDto> ViewVehicleModel(int id);
+    }
+}
