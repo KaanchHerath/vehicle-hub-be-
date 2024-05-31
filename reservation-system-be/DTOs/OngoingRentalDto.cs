5cc020f26eb5e04d298892c7a27e0da9875df843
NavodhSFernando <fernandons.21@uom.lk>
2024-05-31 09:48:34 +0530

    OngoingRental services

--- reservation-system-be/DTOs/OngoingRentalDto.cs
+++ reservation-system-be/DTOs/OngoingRentalDto.cs
@@ -6,6 +6,7 @@ namespace reservation_system_be.DTOs
     (
         int CustomerReservationId,
         string ModelName,
+        string Make,
         DateTime StartDate,
         DateTime EndDate,
         Status Status
