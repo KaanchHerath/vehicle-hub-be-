0e63a1558ac06ba6c61c92a4be32f7e522027433
Kanchana Herath <herathghmk.21@uom.lk>
2024-06-10 10:15:27 +0530

    Made additions of Delete Notifications.

--- reservation-system-be/Services/NotificationServices/INotificationService.cs
+++ reservation-system-be/Services/NotificationServices/INotificationService.cs
@@ -2,9 +2,11 @@ using reservation_system_be.Models;
 
 namespace reservation_system_be.Services.FeedbackServices
 {
-	public interface INotificationService
-	{
-		Task<List<Notification>> GetAllNotifications(int uid);
-	}
+    public interface INotificationService
+    {
+        Task<List<Notification>> GetAllNotifications(int uid);
+
+        Task<bool> DeleteNotification(int notificationId);
+    }
 
 }
