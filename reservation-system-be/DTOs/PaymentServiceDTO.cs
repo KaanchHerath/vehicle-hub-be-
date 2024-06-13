8aa9c51a24a184e84bae81057423c6ac0927f23c
Malith212 <malithnavindu130@gmail.com>
2024-06-13 17:55:01 +0530

    updatepaymentcontroller

--- reservation-system-be/DTOs/PaymentServiceDTO.cs
+++ reservation-system-be/DTOs/PaymentServiceDTO.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using reservation_system_be.Models;
+﻿using reservation_system_be.Models;
+using System;
 
 namespace reservation_system_be.DTOs
 {
-    public record struct PaymentServiceDTO(
-        int Id,
-        string PaymentStatus,
-        string PaymentMethod,
-        DateTime PaymentDate,
-        DateTime PaymentTime,
-        int InvoiceId,
-        Status ReservationStatus // From CustomerReservation related to Invoice
-    );
+    public class PaymentServiceDTO
+    {
+        private Status status;
+
+        public PaymentServiceDTO(int id, string paymentStatus, string paymentMethod, DateTime paymentDate, DateTime paymentTime, int invoiceId, Status status)
+        {
+            Id = id;
+            PaymentStatus = paymentStatus;
+            PaymentMethod = paymentMethod;
+            PaymentDate = paymentDate;
+            PaymentTime = paymentTime;
+            InvoiceId = invoiceId;
+            this.status = status;
+        }
+
+        public int Id { get; set; }
+        public string PaymentStatus { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateTime PaymentTime { get; set; }
+        public int InvoiceId { get; set; }
+        public Status ReservationStatus { get; set; }
+    }
 }
