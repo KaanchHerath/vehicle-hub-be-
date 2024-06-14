9a8882261b06ea32d4f657fb67661398080ee186
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-14 17:11:44 +0530

    bookNowService renamed

--- reservation-system-be/Services/BookNowServices/IBookNowService.cs
+++
@@ -1,9 +0,0 @@
-﻿using reservation_system_be.DTOs;
-
-namespace reservation_system_be.Services.CustomerVehicleServices
-{
-    public interface IBookNowService
-    {
-        Task<IEnumerable<BookNowDto>> GetAllCards();
-    }
-}
