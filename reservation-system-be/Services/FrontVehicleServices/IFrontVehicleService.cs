ece4d263297ffdbca7ec6e34d1ee3870748ee357
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-14 17:11:23 +0530

    FrontVehicleService modified

---
+++ reservation-system-be/Services/FrontVehicleServices/IFrontVehicleService.cs
@@ -0,0 +1,11 @@
+﻿using reservation_system_be.DTOs;
+
+namespace reservation_system_be.Services.CustomerVehicleServices
+{
+    public interface IFrontVehicleService
+    {
+        Task<IEnumerable<BookNowDto>> GetAllCards();
+        Task<VehicleImagesDto> GetImages(int id);
+        Task<CreateAdditionalFeaturesDto> GetAdditionalFeatures(int id);
+    }
+}
