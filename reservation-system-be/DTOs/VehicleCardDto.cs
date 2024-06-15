0f3204cbbc504b430e04af1d9a20cc8ff257c0cc
NavodhSFernando <fernandons.21@uom.lk>
2024-06-15 17:36:30 +0530

    Available vehicles

---
+++ reservation-system-be/DTOs/VehicleCardDto.cs
@@ -0,0 +1,15 @@
+﻿namespace reservation_system_be.DTOs
+{
+    public record struct VehicleCardDto
+    (
+        int Id,
+        string Name,
+        string Make,
+        string Type,
+        int Year,
+        string Transmission,
+        int SeatingCapacity,
+        float CostPerDay,
+        string Thumbnail
+    );
+}
