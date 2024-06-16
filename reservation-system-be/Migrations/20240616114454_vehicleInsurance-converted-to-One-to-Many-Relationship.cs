aa8c58019c6570aa9990ac64f4be56565572744b
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-16 17:17:27 +0530

    Updated VehicleInsurance(One-to-many)

---
+++ reservation-system-be/Migrations/20240616114454_vehicleInsurance-converted-to-One-to-Many-Relationship.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace reservation_system_be.Migrations
+{
+    /// <inheritdoc />
+    public partial class vehicleInsuranceconvertedtoOnetoManyRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleInsurances_VehicleId",
+                table: "VehicleInsurances");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleInsurances_VehicleId",
+                table: "VehicleInsurances",
+                column: "VehicleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleInsurances_VehicleId",
+                table: "VehicleInsurances");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleInsurances_VehicleId",
+                table: "VehicleInsurances",
+                column: "VehicleId",
+                unique: true);
+        }
+    }
+}
