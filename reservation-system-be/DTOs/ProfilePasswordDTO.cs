f4aebfc04c1ff4aaa002a23e4ae723751211f750
Tharushi Samarasinghe <samarasinghesatlt.21@uom.lk>
2024-06-17 17:12:01 +0530

    created the service and the endpoint for resetting the password from profile

---
+++ reservation-system-be/DTOs/ProfilePasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace reservation_system_be.DTOs
+{
+    public class ProfilePasswordDTO
+    {
+        public int Id { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
