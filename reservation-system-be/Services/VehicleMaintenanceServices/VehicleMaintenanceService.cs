09b35d0c9b3536fcc07f5de7a5754d73305eb815
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-22 14:43:27 +0530

    VehicleMaintenance Crud reconfigure

--- reservation-system-be/Services/VehicleMaintenanceServices/VehicleMaintenanceService.cs
+++ reservation-system-be/Services/VehicleMaintenanceServices/VehicleMaintenanceService.cs
@@ -21,9 +21,22 @@ namespace reservation_system_be.Services.VehicleMaintenanceServices
         {
             var vehicleMaintenances = await _context.VehicleMaintenances
             .Include(v => v.Vehicle)
+            .Select(v => new VehicleMaintenanceDto
+            {
+                Id = v.Id,
+                Date = v.Date,
+                Description = v.Description,
+                Type = v.Type,
+                CurrentMileage = v.CurrentMileage,
+                VehicleId = v.VehicleId,
+                RegistrationNumber = v.Vehicle.RegistrationNumber
+            })
+            .OrderByDescending(v => v.Id)
             .ToListAsync();
 
-            if (vehicleMaintenances == null || !vehicleMaintenances.Any())
+            return vehicleMaintenances;
+
+            /*if (vehicleMaintenances == null || !vehicleMaintenances.Any())
             {
                 throw new DataNotFoundException("No vehicle maintenances found");
             }
@@ -47,16 +60,28 @@ namespace reservation_system_be.Services.VehicleMaintenanceServices
                 vehicleMaintenanceDtos.Add(vehicleMaintenanceDto);
             }
             vehicleMaintenanceDtos = vehicleMaintenanceDtos.OrderByDescending(v => v.Id).ToList();
-            return vehicleMaintenanceDtos;
+            return vehicleMaintenanceDtos;*/
         }
 
         public async Task<VehicleMaintenanceDto> GetVehicleMaintenance(int id)
         {
             var vehicleMaintenance = await _context.VehicleMaintenances
                 .Include(v => v.Vehicle)
+                .Select(v => new VehicleMaintenanceDto
+                {
+                    Id = v.Id,
+                    Date = v.Date,
+                    Description = v.Description,
+                    Type = v.Type,
+                    CurrentMileage = v.CurrentMileage,
+                    VehicleId = v.VehicleId,
+                    RegistrationNumber = v.Vehicle.RegistrationNumber
+                })
                 .FirstOrDefaultAsync(v => v.Id == id);
 
-            if (vehicleMaintenance == null)
+                return vehicleMaintenance;
+
+            /*if (vehicleMaintenance == null)
             {
                 throw new DataNotFoundException("Vehicle maintenance not found");
             }
@@ -69,24 +94,35 @@ namespace reservation_system_be.Services.VehicleMaintenanceServices
                 CurrentMileage = vehicleMaintenance.CurrentMileage,
                 Vehicle = await _vehicleService.GetVehicle(vehicleMaintenance.VehicleId)
             };
-            return vehicleMaintenanceDto;
+            return vehicleMaintenanceDto;*/
         }
 
-        public async Task<VehicleMaintenance> CreateVehicleMaintenance(VehicleMaintenance vehicleMaintenance)
+        public async Task<VehicleMaintenance> CreateVehicleMaintenance(CreateVehicleMaintenanceDto CreatevehicleMaintenanceDto)
         {
+            var vehicleMaintenance = new VehicleMaintenance
+            {
+                Date = CreatevehicleMaintenanceDto.Date,
+                Description = CreatevehicleMaintenanceDto.Description,
+                Type = CreatevehicleMaintenanceDto.Type,
+                CurrentMileage = CreatevehicleMaintenanceDto.CurrentMileage,
+                VehicleId = CreatevehicleMaintenanceDto.VehicleId
+            };
+            _context.VehicleMaintenances.Add(vehicleMaintenance);
+            await _context.SaveChangesAsync();
+
             var vehicle = await _context.Vehicles.FindAsync(vehicleMaintenance.VehicleId);
             if(vehicle == null)
             {
                 throw new DataNotFoundException();
             }
             vehicle.Mileage = vehicleMaintenance.CurrentMileage;
-            vehicle.Mileage = vehicleMaintenance.CurrentMileage;
             _context.Entry(vehicle).State = EntityState.Modified;
             await _context.SaveChangesAsync();
+
             return vehicleMaintenance;
         }
 
-        public async Task<VehicleMaintenance> UpdateVehicleMaintenance(int id, VehicleMaintenance vehicleMaintenance)
+        public async Task<VehicleMaintenance> UpdateVehicleMaintenance(int id, CreateVehicleMaintenanceDto vehicleMaintenance)
         {
             var existingVehicleMaintenance = await _context.VehicleMaintenances.FindAsync(id);
             if (existingVehicleMaintenance == null)
