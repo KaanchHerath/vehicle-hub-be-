a86681234580f4ee223d9a2a8f90bbe3160a2154
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-22 14:48:01 +0530

    Reservation Crud Reconfigure

---
+++ reservation-system-be/DTOs/CreateReservationDto.cs
@@ -0,0 +1,15 @@
+﻿using reservation_system_be.Models;
+
+namespace reservation_system_be.DTOs
+{
+    public record struct CreateReservationDto
+    (
+        int Id,
+        TimeOnly StartTime,
+        TimeOnly EndTime,
+        DateTime StartDate,
+        DateTime EndDate,
+        int EmployeeId,
+        Status Status
+    );
+}
